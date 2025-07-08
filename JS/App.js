8e245bfd0bab8f2c3a99cb5e192f09dd413d3ae9
MaccccD <Dumisanimofolo@gmail.com>
2025-07-08 16:50:08 +0200

    Checkbox Functionality Update
    -After asking for help, I get it now. Progress: getting there.
    Key Takeaway: The problem why the method i was using before was not working was bc i was using one checkbox for multiple  tasks that i was adding and thus it made it difficult to track the completion status of each task. So then the solution was; dynamically add a checkbox for each item that gets added as a task. That way it's easier to track its completion status bc its checkbox would be next to it and wouldn't affect the logic for the next tasks that re going to be added .

--- JS/App.js
+++ JS/App.js
@@ -3,7 +3,12 @@ const enterTask = document.getElementById("Enter-Task");
 const addTask = document.getElementById("addTaskBtn");
 const deleteTask = document.getElementById("deleteTaskBtn");
 const markOff = document.getElementById("markComplete");
-const taskItem = document.querySelector('ul');
+const taskItem = document.querySelector("ul");
+const filterItems = document.querySelector("select");
+const allTasks = document.getElementById("allTasks");
+const completedTasks = document.getElementById("completed");
+const incompleteTasks = document.getElementById("incomplete");
+
 
 
 //the array that holds all the tasks added:
@@ -20,7 +25,11 @@ function AddingTasks(){
     //revised method after helping:
     const currentValue = enterTask.value.trim();
     if(currentValue){ // only add the task if the field  is not empty
-        tasksAdded.push(currentValue);
+        tasksAdded.push({ //here i'm adding each task as an object so that it would be easier to track it completion status and to do the filtering fucntionality.
+            id: Date().now,
+            text: currentValue,
+            completed: false
+        });
         console.log("Task has been added to the array: ", currentValue);
         DisplayTaskItem();
         StorageUpdate();
@@ -31,24 +40,73 @@ function AddingTasks(){
 function DisplayTaskItem(){
     //show the task that has been added as an html list item:
     taskItem.innerHTML = ''; //clear existing
-    tasksAdded.map((task, index)=>{ // here i'm basically creating a list item as an element so that each task added is its own list item and has a unique index
+    tasksAdded.forEach((task, index)=>{ // here i'm basically creating a list item as an element so that each task added is its own list item and has a unique index
         const li = document.createElement('li'); 
-        li.textContent = task;
+        li.innerText = task; // can also use Text Conten
         li.setAttribute('data-index', index);
+
+
+        //create individual checkboxes instead of one global checkbox:
+        const checkbox = document.createElement("input");
+        checkbox.classList.add("markComplete");
+        checkbox.type = "checkbox";
+        checkbox.style.backgroundColor = "green";
+        checkbox.checked = task.completed;
+        checkbox.addEventListener("change", ()=>{ToggleTask(index)})
+       
+
+        //create span text that will be linked wih the task item that has been added
+        const textSpan = document.createElement("span");
+        textSpan.textContent = task.text;
+        if(task.completed){
+            textSpan.style.textDecoration = "line-through";
+        }
+
+        li.appendChild(textSpan);
+        li.appendChild(checkbox)
         taskItem.appendChild(li);
+
+
     })
+}
+
+function ToggleTask(index){ //here i'm checking if the task is completed by tracking its completion status vs its incomplete status
+    tasksAdded[index].completed = !tasksAdded[index].completed;
+    DisplayTaskItem();
+    StorageUpdate();
 }
 
 function DeleteTasks(){
    deleteTask.addEventListener('click', function(){
     tasksAdded.pop();//removes an element from the array.
-    taskItem.innerText = ""; // removes evrythinggg
+    taskItem.textContent = ""; // removes evrythinggg
     console.log("delete works!");
    })
 }
 
+
+
+
+function FilterTasks(){
+    filterItems.addEventListener('change', function(){
+        tasksAdded.filter(function(task){
+         if(task=== incompleteTasks){
+           //show the items that have just been added as tasks 
+           //here i just dk how to link the added tasks into the specific groups i have created with theirn unique id.
+         }
+         else{
+            return `${"No task has been added yet!"}`;
+         }
+
+        })
+       
+      
+    })
+}
+
 function StorageUpdate(){
     localStorage.setItem("AddedTasks", JSON.stringify(tasksAdded)); // adding the task that has been typed into the local storage
+    console.log(tasksAdded);
 }
 
 
@@ -59,3 +117,4 @@ function SubmitTasks(){
 } 
 SubmitTasks();
 DeleteTasks();
+//FilterTasks();
