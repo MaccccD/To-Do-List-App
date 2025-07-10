8a50e9860c9d96781179cb6bbdd11cee96f9f2e6
MaccccD <Dumisanimofolo@gmail.com>
2025-07-10 20:04:17 +0200

    Weather Dashboard Search Functionality Update Part 3
    -Implemented a search feature with no aid!! and it works!

--- JS/Weather.js
+++ JS/Weather.js
@@ -1,19 +1,19 @@
 //collecting refrences:
 const displayWeather = document.getElementById("display-Weather");
+const searchingItems = document.getElementById("searching");
 const searchCity = document.getElementById("searchCity");
 
-
-//Fetch data from an external API:
-async function FetchWeatherData(){
-    try {
-        const weatherData = [{
-          location: "Alberton, 1448",
-          temperature: 12,
+const weatherData = [{ //created my own local data
+          Location: "Alberton, 1448",
+          Temperature: 12,
           Precipitation: 0,
           Humidity: 50,
           Wind: 5,
           Forecast: "Clear Skies"
         }];
+ async function FetchWeatherData(){
+    try {
+        
          DisplayWeatherData(weatherData);
         // const response = await fetch(weatherData); //comented it put bc the fetch expects a url not an array and i diecided to create my own data
         // if(!response.ok){
@@ -35,8 +35,8 @@ function DisplayWeatherData(dataInfo){
   displayWeather.innerHTML = "";
   dataInfo.forEach(data => {
   const weatherCard = document.createElement("div");
-  weatherCard.innerHTML = `<h1>Location: ${data.location}</h1>
-   <p>Temperature: ${data.temperature}</p>
+  weatherCard.innerHTML = `<h1>Location: ${data.Location}</h1>
+   <p>Temperature: ${data.Temperature}</p>
    <p>Forecast: ${data.Forecast}</p>
    <p>Precipitation: ${data.Precipitation}%</p>
    <p>Humidity: ${data.Humidity}%</p>
@@ -51,5 +51,47 @@ function DisplayWeatherData(dataInfo){
 
 //searching by city functionality:
 function Search(){
+searchCity.addEventListener("change",function(){
+const searchedItem = this.value;
+let filteredItems = [];
+switch(searchedItem){
+   case "Location":
+    filteredItems = weatherData.map(data=> data.Location);
+    break;
+   case "temperature":
+    filteredItems = weatherData.map(data => data.Temperature);
+    break;
+   case "Precipitation":
+      filteredItems = weatherData.map(data => data.Precipitation);
+      break;
+   case "Humidity":
+      filteredItems = weatherData.map(data=> data.Humidity);
+      break;
+   case "Wind": 
+      filteredItems = weatherData.map(data => data.Wind);
+      break;
+    case "Forecast":
+      filteredItems = weatherData.map(data => data.Forecast);
+      break;
+ }
+
+  DisplaySearchedItems(filteredItems);
+  console.log(filteredItems);
+
+ });
+ }
+ 
+ Search();
+
+
+function DisplaySearchedItems(searchedItems){
+    searchingItems.innerHTML = "";
+    searchedItems.forEach(itemSearched => {
+    const searchedItem = document.createElement("div");
+    searchedItem.textContent = itemSearched;
+    searchedItem.style.color = "green";
+    searchingItems.appendChild(searchedItem);
+    console.log("working, yayyy! bc each searched item is showing as intended");
+  });
 
 }
