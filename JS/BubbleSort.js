5eeaa3cd84ce31cb5d19a60ed9d94fa25022d998
MaccccD <Dumisanimofolo@gmail.com>
2025-07-18 21:09:41 +0200

    Bubble Sort Functionality Update Part 1
    -Challenge: showing the numbers as boxes and then swapping them around by comparing them.
    Progress: the numbers display as boxes. Struggled at first but that was bc the class i was adding was not in the css script. Figured that out after googling.
    Still to complete: Numbers displaying as swapped in the original box design. R.n they do swap but i' trying to figure out how to show the two numbers have been swapped.

---
+++ JS/BubbleSort.js
@@ -0,0 +1,55 @@
+const bubbleSort = document.getElementById("bubble-Sort");
+const swapNumbers = document.getElementById("swapNumbers");
+
+
+//the array of numbers :
+const numbers = [56, 34, 78, 90, 98, 67, 88, 12, 88, 10];
+
+
+
+//display the numbers as boxes:
+function DisplayNumbers(){
+    bubbleSort.innerHTML = "";
+    numbers.forEach(number => {
+     const boxes = document.createElement("div");
+     boxes.classList.add("number-box");
+     boxes.textContent = number;
+     bubbleSort.appendChild(boxes);
+     console.log(boxes);
+    });
+}
+DisplayNumbers();
+
+function SwapNumbers(){
+let index0 = 0;
+let index1 = 1;
+let index2 = 2;
+let index3 = 3;
+let index4 = 4;
+let index5 = 5;
+let index6 = 6;
+let index7 = 7;
+let index8 = 8;
+let index9 = 9;
+  swapNumbers.addEventListener("click", function(){
+//     if(numbers[index0] > numbers[index1]) {
+//        numbers[index1], numbers[index0];
+//        console.log("numbers have swappped");
+//   }
+  numbers[index0], numbers[index1] = numbers[index1], numbers[index0];
+  console.log("numbers have swappped");
+  DisplaySwappedNumbers([numbers[index1]], numbers[index0]);
+  })
+}
+SwapNumbers();
+
+function DisplaySwappedNumbers(swappedNumbers){
+    bubbleSort.innerHTML = "";
+    swappedNumbers.forEach(num => {
+    const swappedSpan = document.createElement("span");
+    swappedSpan.innerText = num;
+    swappedSpan.style.color = "yellow";
+    bubbleSort.appendChild(swappedSpan);
+    console.log(swappedNumbers);
+   });
+}
