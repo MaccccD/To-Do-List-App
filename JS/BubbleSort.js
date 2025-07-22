8346a07361380469d7ed61b85fc4d28bcd8c4c87
MaccccD <Dumisanimofolo@gmail.com>
2025-07-23 01:26:01 +0200

    Conditional Swap Logic Functionality Update
    -Fixed the conditional or validate swap logic.
    -Key Takeaway: Found out that i was using "IndexOf" incorrectly bc with the way it was used, the values would most likely always not swap bc "IndexPf" is meant to be used to find a specific value at a specified index within the array , not to actually access it. So with the way i had it; it would never find 32 at index 0 or 56 at index one. It would always return either a "-1"  or "0". Therefore , the correct way to access the value of the numbers in the "numbers" array was to use "array[specific index]" and then compare them.

--- JS/BubbleSort.js
+++ JS/BubbleSort.js
@@ -70,14 +70,16 @@ function DisplaySwappedNumbers(){
 
 function SortNumbers (){
  validateSwap.addEventListener("click", function(){
-   if(numbers.indexOf(32, 0) > numbers.indexOf(56, 1)){
-  //  numbers[index1], numbers[index0] = numbers[index0], numbers[index1];
+  // if(numbers.indexOf(32, 0) > numbers.indexOf(56, 1)){ //after seeking aid: i wasn't supposed to use "indexOf" here bc it wont work for most cases
+  //  bc IndexOf only tries to find the value at that index but it does not access the actual value.
+  if(numbers[1] > numbers[0]){
+    numbers[index1], numbers[index0] = numbers[index0], numbers[index1];
    DisplaySwappedNumbers(numbers[index0], numbers[index1]);
     console.log("numbers swapped !!");
   }
   else{
     alert("Numbers won't swap bc the first number is not greater than the second number!");
-    feedbackTxt.innerHTML = `<h1 style = "color: red">Numbers did not swap</h1>`;
+    feedbackTxt.innerHTML = `<h1 style = "color: red">Values did not swap</h1>`;
   }
  })
 }
