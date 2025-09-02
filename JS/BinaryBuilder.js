5087a7896e1dc88601bca32b0942d42e24c68862
MaccccD <Dumisanimofolo@gmail.com>
2025-09-02 23:56:40 +0200

    Binary Number to Letter Logic Update Part 1

--- JS/BinaryBuilder.js
+++ JS/BinaryBuilder.js
@@ -1,1 +1,18 @@
 const binaryNumbers = document.getElementById("binaryNumbers");
+
+
+let binaryWords = [
+    {letter: "A",
+     binary: "010101"},
+     {letter: "B",
+        binary: "021092"
+     },
+     {},
+     {},
+    ];
+
+function BinaryNumbers(){
+    binaryNumbers.addEventListener("input", function(){
+    //comng right up!
+    });
+}
