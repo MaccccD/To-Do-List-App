c8f56d045d4483987aed04aff824e6bf859a2c96
MaccccD <Dumisanimofolo@gmail.com>
2025-09-04 23:00:33 +0200

    Binary Number  to Letter Logic Small Update

--- JS/BinaryBuilder.js
+++ JS/BinaryBuilder.js
@@ -5,9 +5,11 @@ let binaryWords = [
     {letter: "A",
      binary: "010101"},
      {letter: "B",
-        binary: "021092"
+      binary: "021092"
      },
-     {},
+     {letter: "C",
+        binary: "012345"
+     },
      {},
     ];
 
