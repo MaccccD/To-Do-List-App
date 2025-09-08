afefe5e1112829e15ff8a1dd7938afeb678e3b77
MaccccD <Dumisanimofolo@gmail.com>
2025-09-08 12:36:34 +0200

    Binary Letters & Numbers Display Logic Update
    -added logic to show binary numbers mapped to each specific letter.

--- JS/BinaryBuilder.js
+++ JS/BinaryBuilder.js
@@ -1,20 +1,51 @@
 const binaryNumbers = document.getElementById("binaryNumbers");
-
+const binaryLetters = document.getElementById("binaryLetters");
 
 let binaryWords = [
-    {letter: "A",
-     binary: "010101"},
+     {letter: "A",
+      binary: "01000001"},
      {letter: "B",
-      binary: "021092"
+      binary: "01000010"
      },
      {letter: "C",
-        binary: "012345"
+      binary: "01000011"
      },
-     {},
+     {letter: "D",
+      binary: "01000100"
+     },
+     {letter: "E",
+      binary: "01000101"
+     },
+     {
+     }
     ];
 
-function BinaryNumbers(){
-    binaryNumbers.addEventListener("input", function(){
-    //comng right up!
+function ShowBinaryNumbers(){
+     binaryNumbers.innerHTML = "";
+
+     binaryWords.forEach(binary => {
+        let binaryNum = document.createElement("div");
+        binaryNum.textContent = binary.binary;
+        binaryNum.style.accentColor = "green";
+        binaryNum.classList.add("binary-box")
+        binaryNumbers.appendChild(binaryNum);
+        console.log(binaryNum);
+
     });
 }
+ShowBinaryNumbers();
+
+function ShowBinaryLetter(){
+        binaryLetters.innerHTML = "";
+
+        binaryWords.forEach(binaryW => {
+             let binaryLet = document.createElement("div");
+             binaryLet.textContent = binaryW.letter;
+             binaryLet.style.accentColor = "orange";
+           //  binaryLet.classList.add("binary-box");
+             binaryNumbers.appendChild(binaryLet);
+             console.log(binaryLet);
+        });
+    
+    }
+   ShowBinaryLetter();
